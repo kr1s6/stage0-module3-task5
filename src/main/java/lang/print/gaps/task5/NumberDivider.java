c9a3438a086fd354d4c3640f449fb070c887e508
kr1s6 <krzysztof.bielkiewicz@interia.pl>
2023-05-06 23:40:07 +0200

    Finished.

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double x = toBeDivided / 5.0;
+        System.out.println(x);
+
     }
 }
--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        byte first = 1;
+        short second = 10;
+        short third = 100;
+        System.out.println(first + String.valueOf(second) + third);
     }
 }
